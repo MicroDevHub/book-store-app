240b00e0fc19da825692be147a6a9ea2a75fef77
huyhoang1001 <huyhoangpro1001@gmail.com>
2022-01-01 14:51:30 +0700

    Init auth service and set up infra for project

---
+++ auth/src/index.ts
@@ -0,0 +1,1 @@
+import express from 'express';import { json } from 'body-parser';const app = express();app.use(json());app.get("/api/users/currentuser", (req, res) => {    res.send("Hi there!");});app.listen(3000, () => {    console.log('Auth service is listening on port 3000!');})
