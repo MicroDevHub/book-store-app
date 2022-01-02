ee3d45a146ea730c45f22bc4f8f169b120083de5
huyhoang1001 <huyhoangpro1001@gmail.com>
2022-01-02 19:17:57 +0700

    Added mongodb for auth service and set up mongo container on infra

---
+++ auth/src/errors/bad-request-error.ts
@@ -0,0 +1,1 @@
+import { BaseError } from "./base-error";export class BadRequestError extends BaseError {    statusCode = 400;    constructor(public message: string) {        super(message);        // Just for logging on server        Object.setPrototypeOf(this, BadRequestError.prototype);    }    serializeErrors() {        return [{ message: this.message }];    }}
--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,1 +1,1 @@
-import express from 'express';const router = express.Router();router.get('/api/users/currentuser', (req, res) => {    console.log()});export { router as currentuserRouter };
+import express from 'express';const router = express.Router();router.get('/api/users/currentuser', (req, res) => {    res.send('Hi there');});export { router as currentuserRouter };
