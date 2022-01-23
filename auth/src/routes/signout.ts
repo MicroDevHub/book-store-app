ef894ad9ae2a142ec9c335f7438af5ee3d55a90b
huyhoang1001 <huyhoangpro1001@gmail.com>
2022-01-23 21:00:20 +0700

    Implemented testing strategies in microservice

--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -1,1 +1,1 @@
-import express from 'express';const router = express.Router();router.get('/api/users/signout', (req, res) => {    req.session = null;    res.send({});});export { router as signoutRouter };
+import express from 'express';const router = express.Router();router.post('/api/users/signout', (req, res) => {    req.session = null;    res.send({});});export { router as signoutRouter };
