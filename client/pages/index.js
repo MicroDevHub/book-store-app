f00812d613a6d78915aab3c01652cf4cc209b877
huyhoang1001 <huyhoangpro1001@gmail.com>
2022-02-27 17:22:25 +0700

    Completed integrating a Server Side-Rendered React App with NextJs

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,1 +1,1 @@
-const Landing = () => {  return <h1>Landing Page</h1>;};export default Landing;
+import buildClient from '../api/build-client';const LandingPage = ({ currentUser }) => {  console.log(currentUser);  return currentUser ? (    <h1>You are signed in</h1>  ) : (    <h1>You are NOT signed in</h1>  );};LandingPage.getInitialProps = async ( context ) => {  const client = buildClient(context);  const { data } = await client.get('/api/users/currentuser');  return data;}export default LandingPage;
